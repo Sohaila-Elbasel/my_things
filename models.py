b705d8855534fe14057b2b3a68e28532a5b0456f
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-08-21 15:24:24 +0200

    deploy files

---
+++ models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Category(db.Model):
+    __tablename__ = "category"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+class Book(db.Model):
+    __tablename__ = "book"
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.relationship("Category", backref="book", lazy=True)
+    name = db.Column(db.String(250), nullable=False)
+    price = db.Column(db.Float(5), nullable=False)
+    rate = db.Column(db.Float(3))
+    height = db.Column(db.Float(5))
+    width = db.Column(db.Float(5))
+    depth = db.Column(db.Float(5))
+    weight = db.Column(db.Float(5))
+    author = db.Column(db.String(50), nullable=False)
+    release_date = db.Column(db.DateTime())
+    summary = db.Column(db.String(), nullable=False)
+    about_author = db.Column(db.String())
+    age_range = db.Column(db.String(5))
+    pages = db.Column(db.String(5))
+    publisher = db.Column(db.String(50))
+    language = db.Column(db.String(20))
+    type = db.Column(db.String(50))
+    condition = db.Column(db.String(25))
