9895db5b13cf725e4f8124358f167e3af54ef67d
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-08-28 23:53:18 +0200

    filter and search in nav bar

--- app.py
+++ app.py
@@ -1,89 +1,114 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import SearchForm, Filter, User
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dca5f91bd1299c2a4a2db9491840eb05'
-engine = create_engine('postgres://hqethunzcsnxmp:980d9b1b73b638c2eeb2463a40de1885115eacf6d6d484840d876fb6d659c889@ec2-34-238-26-109.compute-1.amazonaws.com:5432/df6alckgo2voap')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://hqethunzcsnxmp:980d9b1b73b638c2eeb2463a40de1885115eacf6d6d484840d876fb6d659c889@ec2-34-238-26-109.compute-1.amazonaws.com:5432/df6alckgo2voap'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 books = [
+        {'name': 'Life of Pi',
+         'author': 'Yann Martel',
+         'published': ' 2003',
+         'category': 'action',
+         'img_url': 'https://images-na.ssl-images-amazon.com/images/I/51rxEvLljUL._SX322_BO1,204,203,200_.jpg'
+        },
+        {'name': 'The Three Musketeers',
+         'author': ' Alexandre Dumas',
+         'published': '2014',
+         'category': 'action',
+         'img_url': 'https://images-na.ssl-images-amazon.com/images/I/41TxXqToCCL._SX348_BO1,204,203,200_.jpg'
+        },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'classics',
+         'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
+        },
+        {'name': '1984',
+         'author': 'George Orwell',
+         'published': '1949',
+         'category': 'classics',
+         'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
+        },
+        {'name': '1984',
+         'author': 'George Orwell',
+         'published': '1949',
+         'category': 'Comic Book',
+         'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
+        },
+        {'name': '1984',
+         'author': 'George Orwell',
+         'published': '1949',
+         'category': 'horror',
          'img_url': 'https://kbimages1-a.akamaihd.net/fb0c52e7-c427-4eb3-b5aa-9aafc7efea43/353/569/90/False/AhIbw1TJuje1l6QPMtht5A.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'horror',
          'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'horror',
          'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'horror',
          'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'historical',
          'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'historical',
          'img_url': 'https://kbimages1-a.akamaihd.net/fb0c52e7-c427-4eb3-b5aa-9aafc7efea43/353/569/90/False/AhIbw1TJuje1l6QPMtht5A.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'historical',
          'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'fantasy',
          'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
+         'category': 'fantasy',
          'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
         },
         {'name': '1984',
          'author': 'George Orwell',
          'published': '1949',
-         'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
-        },
-        {'name': '1984',
-         'author': 'George Orwell',
-         'published': '1949',
-         'img_url': 'https://kbimages1-a.akamaihd.net/fb0c52e7-c427-4eb3-b5aa-9aafc7efea43/353/569/90/False/AhIbw1TJuje1l6QPMtht5A.jpg'
-        },
-        {'name': '1984',
-         'author': 'George Orwell',
-         'published': '1949',
-         'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
-        },
-        {'name': '1984',
-         'author': 'George Orwell',
-         'published': '1949',
-         'img_url': 'https://images-na.ssl-images-amazon.com/images/I/81WunXo0giL.jpg'
-        },
-        {'name': '1984',
-         'author': 'George Orwell',
-         'published': '1949',
-         'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
-        },
-        {'name': '1984',
-         'author': 'George Orwell',
-         'published': '1949',
+         'category': 'fantasy',
          'img_url': 'https://kbimages1-a.akamaihd.net/e39ed975-600f-49c7-b29d-5d531f24f09f/353/569/90/False/dSCAQLyKPDObZmRnq4PCYQ.jpg'
         },
 
 ]
+
+def search(book_name, list):
+    result = []
+    for book in list:
+        if book_name.lower() in book['name'].lower():
+            result.append(book)
+    return result
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -94,6 +119,19 @@ def home():
 def browse():
     form = SearchForm()
     filters = Filter()
+
+    if filters.go.data:
+        filter_list = []
+        for book in books:
+            if book['category'] == filters.filter.data:
+                filter_list.append(book)
+        return render_template('browse.html', title=filters.filter.data, form= form, books = filter_list, browse='browse', filters = filters)
+
+    elif form.search_btn.data:
+        result = []
+        result = search(form.keywords.data, books)
+        return render_template('browse.html', title='form.search_btn.data', form= form, books = result, browse='browse', filters = filters)
+
     return render_template('browse.html', title='Browse Books', form= form, books = books, browse='browse', filters = filters)
 
 @app.route('/books/<string:name>', methods=['POST', 'GET'])
--- models.py
+++ models.py
@@ -1,17 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 
-db = SQLAlchemy()
 
 
 class Category(db.Model):
     __tablename__ = "category"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(35), nullable=False)
+    books = db.relationship("Book", backref="book", lazy= True)
 
 class Book(db.Model):
     __tablename__ = "book"
     id = db.Column(db.Integer, primary_key=True)
-    category_id = db.relationship("Category", backref="book", lazy=True)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable = False)
     name = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Float(5), nullable=False)
     rate = db.Column(db.Float(3))
@@ -28,8 +28,11 @@ class Book(db.Model):
     publisher = db.Column(db.String(50))
     language = db.Column(db.String(20))
     condition = db.Column(db.String(25))
+    photo_url = db.Column(db.String)
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(), primary_key = True)
+    username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
